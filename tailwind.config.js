b1b3238c70f978d67e16dcc4bcbf95f12f02a45e
dezoul <aussieme@gmail.com>
2024-09-03 10:11:45 +0800

    init

---
+++ tailwind.config.js
@@ -0,0 +1,63 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './_includes/**/*.{html,js,md}',
+    './_layouts/**/*.{html,js,md}',
+    './_*/*.{html,js,md}',
+    './*.{html,js,md}'
+  ],
+  theme: {
+    fontSize: {
+      xs: ['0.75rem', '1.2'],
+      sm: ['0.875rem;', '1.2'],
+      base: ['1rem', '1.2'],
+      lg: ['1.125rem;', '1.2'],
+      xl: ['1.25rem', '1.2'],
+      '2xl': ['1.5rem', '1.2'],
+      '3xl': ['1.875rem', '1'],
+      '4xl': ['2.25rem', '1'],
+      '5xl': ['3rem', '1'],
+      '6xl': ['3.75rem', '1'],
+      '7xl': ['4.5rem', '1'],
+      '8xl': ['6rem', '1'],
+      '9xl': ['8rem', '1'],
+    },
+    fontFamily: {
+      'sans': ['proxima-nova', 'ui-sans-serif'],
+      'sans-tight': ['proxima-nova-condensed', 'ui-sans-serif'],
+      'mono': ['Inconsolata', 'ui-mono', 'monospace'],
+      'soft': ['proxima-soft', 'ui-sans-serif'],
+      'soft-tight': ['proxima-soft-condensed', 'ui-sans-serif'],
+    },
+    extend: {
+      lineHeight: {
+        'tight': '.85',
+        'snug': '.95',
+        '12': '3rem',
+      },
+      spacing: {
+        '5p': '5%',
+        '10p': '10%',
+        '15p': '15%',
+      },
+      aspectRatio: {
+        '5/3': '5 / 3',
+      },
+      minHeight:{
+        '100': '100vh',
+        '90': '90vh',
+        '80': '80vh',
+        '70': '70vh',
+        '60': '60vh',
+        '50': '50vh',
+      },
+      aspectRatio: {
+        'landscape': '3 / 2',
+        'portrait-tall': '5/8',
+        'portrait': '4/5',
+      },
+    },
+  },
+  plugins: [
+  ],
+}
